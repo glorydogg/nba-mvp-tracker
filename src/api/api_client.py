bd4c917785d83a1654a9d9808aebb432da8b848b
Kanan Dimanche <kdimanche1@gmail.com>
2025-12-29 19:30:16 -0500

    Client request fix

--- src/api/api_client.py
+++ src/api/api_client.py
@@ -11,11 +11,15 @@ class NBAClient:
         self.season = season
         self.headers = {
             'Host': 'stats.nba.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Accept': 'application/json, text/plain, */*',
             'Accept-Language': 'en-US,en;q=0.5',
             'Referer': 'https://www.nba.com/',
             'Origin': 'https://www.nba.com',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
 }
 
 
@@ -30,7 +34,7 @@ class NBAClient:
                     league_id_nullable='00',             
                     measure_type_detailed_defense='Base',
                     per_mode_detailed='PerGame',   # This will actually give you PPG/RPG directly!
-                    timeout=30)
+                    timeout=59)
                 df = stats.get_data_frames()[0]
 
                 logger.info(f"Succesfully  retrieved data on attempt {i + 1}\n")
